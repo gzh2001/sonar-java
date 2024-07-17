e0074b6f6bae7f92402e08c1aecf62fe57cc0c74
Gaofan <gzhdev@outlook.com>
2024-07-17 10:54:35 +0800

    1.0.2 关闭分隔符校验和ip校验

--- docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/checks/HardEncodedWebURICheck.java
+++ docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/checks/HardEncodedWebURICheck.java
@@ -43,7 +43,7 @@ public class HardEncodedWebURICheck extends IssuableSubscriptionVisitor {
   private static final String URI_REGEX = String.format("^%s://.+", SCHEME);
   private static final String IP_REGEX = "^([0-9]{1,3}\\.){3}[0-9]{1,3}(:[0-9]{1,5})?(/.*)?$";
 
-  private static final Pattern URI_PATTERN = Pattern.compile(URI_REGEX + "|" + IP_REGEX);
+  private static final Pattern URI_PATTERN = Pattern.compile(URI_REGEX);
   private static final Pattern VARIABLE_NAME_PATTERN = Pattern.compile("filename|path", Pattern.CASE_INSENSITIVE);
   private static final Pattern PATH_DELIMETERS_PATTERN = Pattern.compile("\"/\"|\"//\"|\"\\\\\\\\\"|\"\\\\\\\\\\\\\\\\\"");
 
