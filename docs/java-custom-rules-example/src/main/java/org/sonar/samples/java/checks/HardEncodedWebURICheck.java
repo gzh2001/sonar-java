268840f35d0f7671a389930594e73fa847984fb6
Gaofan <gzhdev@outlook.com>
2024-10-10 17:04:00 +0800

    1.1.1 debug-2 过滤回环地址

--- docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/checks/HardEncodedWebURICheck.java
+++ docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/checks/HardEncodedWebURICheck.java
@@ -42,7 +42,9 @@ public class HardEncodedWebURICheck extends IssuableSubscriptionVisitor {
   private static final Pattern PATH_DELIMETERS_PATTERN = Pattern.compile("\"/\"|\"//\"|\"\\\\\\\\\"|\"\\\\\\\\\\\\\\\\\"");
 //  排除回环ip
   private static final String IPV4_LOOPBACK_URI_REGEX = String.format("^%s://127\\.0\\.0\\.1(:[0-9]{1,5})?(/.*)?$", SCHEME);
-  private static final Pattern IPV4_LOOPBACK_URI_PATTERN = Pattern.compile(IPV4_LOOPBACK_URI_REGEX);
+  private static final String IPV4_LOOPBACK_REGEX = "^127\\.0\\.0\\.1(:[0-9]{1,5})?(/.*)?$";
+
+  private static final Pattern IPV4_LOOPBACK_URI_PATTERN = Pattern.compile(IPV4_LOOPBACK_URI_REGEX + '|' + IPV4_LOOPBACK_REGEX);
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
