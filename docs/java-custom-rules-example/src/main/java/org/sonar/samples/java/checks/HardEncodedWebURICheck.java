f156692e16dd12057dd837c07d276c68f5257bee
Gaofan <gzhdev@outlook.com>
2024-11-08 10:28:58 +0800

    1.1.3_d5 构造入口日志

--- docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/checks/HardEncodedWebURICheck.java
+++ docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/checks/HardEncodedWebURICheck.java
@@ -103,7 +103,7 @@ public class HardEncodedWebURICheck extends IssuableSubscriptionVisitor {
 
   private void checkAssignment(AssignmentExpressionTree tree) {
     String msg = ((LiteralTree) Objects.requireNonNull(tree.expression())).value();
-    log.info("checkVariable:{}", tree);
+    log.info("checkAssignment:{}", tree);
     // 检查赋值表达式是否涉及文件名或路径变量，并且不属于注释的一部分
     if (isFileNameVariable(getVariableIdentifier(tree)) && !isPartOfAnnotation(tree)) {
       log.info("checkAssignment isFileNameVariable");
