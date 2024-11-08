73c848c47d8f6d1fd6c07b4b6b803e57895a23b9
Gaofan <gzhdev@outlook.com>
2024-11-08 10:05:22 +0800

    1.1.3_d5 构造入口日志

--- docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/checks/HardEncodedWebURICheck.java
+++ docs/java-custom-rules-example/src/main/java/org/sonar/samples/java/checks/HardEncodedWebURICheck.java
@@ -94,7 +94,7 @@ public class HardEncodedWebURICheck extends IssuableSubscriptionVisitor {
 
   private void checkVariable(VariableTree tree) {
     // 检查变量名是否匹配文件名或路径模式
-    log.info("checkVariable:{}",((LiteralTree)tree.initializer()).value());
+    log.info("checkVariable:{}",((LiteralTree) Objects.requireNonNull(tree.initializer())).value());
     if (isFileNameVariable(tree.simpleName())) {
       log.info("checkVariable isFileNameVariable");
       checkExpression(tree.initializer());
